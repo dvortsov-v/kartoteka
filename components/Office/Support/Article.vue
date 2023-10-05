0136ddfc156dd960983c5f3fba43137fdc0ea03d
vladislav_dvortsov <dvortsov.vlad@gmail.com>
2023-10-05 21:22:00 +0300

    Страница Поддержки

---
+++ components/Office/Support/Article.vue
@@ -0,0 +1,44 @@
+<template>
+    <details class="support-article">
+        <summary @click="handleToogle" class="support-article__head">
+            <span class="support-article__number">№ {{ number }}</span>
+            <h5 class="support-article__title">{{ title }}</h5>
+            <span class="support-article__status">Активно</span>
+            <svg-icon class="support-article__icon" :name="iconAccordion" />
+        </summary>
+        <div class="support-article__body">
+            <div class="support-article__row">
+                <span class="support-article__name">Вопрос</span>
+                <div class="support-article__text">
+                    <p>На маркетплейсе не могу выбрать способ оплаты чтоб разместить заявку</p>
+                </div>
+            </div>
+            <div class="support-article__row support-article__row--answer">
+                <span class="support-article__name">Ответ</span>
+                <div class="support-article__text">
+                    <p>На маркетплейсе обычно предлагают несколько способов оплаты для удобства покупателей. Некоторые из наиболее распространенных способов оплаты включают:</p>
+                    <p><b>Оплата банковской картой:</b> это один из наиболее удобных и популярных способов оплаты. Покупатель может использовать кредитную или дебетовую карту для оплаты покупки. Маркетплейс может принимать различные типы кредитных карт, такие как Visa, Mastercard, American Express и другие.</p>
+                    <p><b>Электронные кошельки:</b> некоторые маркетплейсы также предлагают оплату через электронные кошельки, такие как PayPal, Яндекс.Деньги, WebMoney, QIWI и т.д.</p>
+                </div>
+            </div>
+        </div>
+    </details>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    title: String,
+    text: String,
+    number: String,
+})
+const isOpen: Ref<boolean> = ref(false);
+
+const handleToogle = () => {
+    isOpen.value = !isOpen.value;
+}
+const iconAccordion = computed(() => unref(isOpen) ? 'minus' : 'plus');
+</script>
+
+<style scoped lang="scss">
+@import "@/components/Office/Support/styles/support-article.scss";
+</style>
--- pages/office/support.vue
+++ pages/office/support.vue
@@ -1,46 +1,52 @@
 <template>
-    <OfficeLayout title="Заявки" countObjects="25" class="orders-page">
-        <main class="orders-page__main">
-            <div class="orders-page__filters">
-                <ul class="orders-page__tabs orders-page-tabs">
-                    <li v-for="tab in tabs" :key="`orders-page-tabs-${tab.id}`" class="orders-page-tabs__item">
-                        <div class="orders-page-tabs__tab orders-tabs">
-                            <UiChoices v-model:checked="activeTab" @update:checked="handleChoice" :value="tab.id" name="view" class="orders-tabs__choice" inputType="radio">
+    <OfficeLayout title="Поддержка" countObjects="25" class="support-page">
+        <main class="support-page__main">
+            <div class="support-page__filters">
+                <ul class="support-page__tabs support-page-tabs">
+                    <li v-for="tab in tabs" :key="`support-page-tabs-${tab.id}`" class="support-page-tabs__item">
+                        <div class="support-page-tabs__tab support-tabs">
+                            <UiChoices v-model:checked="activeTab" @update:checked="handleChoice" :value="tab.id" name="view" class="support-tabs__choice" inputType="radio">
                                 <template #visibleElement="{isChecked}">
                                     <div
                                         :class="classesTabs(isChecked)"
-                                        class="orders-tabs__wrap"
+                                        class="support-tabs__wrap"
                                     >
-                                        <span class="orders-tabs__text">{{tab.name}}</span>
-                                        <span class="orders-tabs__count">15</span>
+                                        <span class="support-tabs__text">{{tab.name}}</span>
                                     </div>
                                 </template>
                             </UiChoices>
                         </div>
                     </li>
                 </ul>
-                <CommonViewsSetting class="orders-page__view" @change="changeViews" />
             </div>
-            <CommonProductList
-                :listProducts="productsStore.products"
-                :isCompactedView="isCompactedView"
-                isProductInOrder
-                class="orders-page__products"
-            />
-            <div class="orders-page__navigation orders-page-navigation">
-                <UiPagination countPage="5" class="orders-page-navigation__pagination" />
-                <UiButton theme="transparent" class="orders-page-navigation__more">
+            <ul class="support-page__appeals">
+                <li class="support-page__item">
+                    <OfficeSupportArticle number="2356" title="Мои заявки" class="support-page__article" />
+                </li>
+                <li class="support-page__item">
+                    <OfficeSupportArticle number="2356" title="Мои заявки" class="support-page__article" />
+                </li>
+                <li class="support-page__item">
+                    <OfficeSupportArticle number="2356" title="Мои заявки" class="support-page__article" />
+                </li>
+                <li class="support-page__item">
+                    <OfficeSupportArticle number="2356" title="Мои заявки" class="support-page__article" />
+                </li>
+            </ul>
+            <div class="support-page__navigation support-page-navigation">
+                <UiPagination countPage="5" class="support-page-navigation__pagination" />
+                <UiButton theme="transparent" class="support-page-navigation__more">
                     <svg-icon
                         name="adding"
-                        class="orders-page-navigation__icon"
+                        class="support-page-navigation__icon"
                     />
-                    <span class="orders-page-navigation__text">
+                    <span class="support-page-navigation__text">
                                 Показать ещё
                             </span>
                 </UiButton>
-                <span class="orders-page-navigation__show orders-page-navigation-show">
-                            <span class="orders-page-navigation-show__text">Показано:</span>
-                            <span class="orders-page-navigation-show__count">50 из 120</span>
+                <span class="support-page-navigation__show support-page-navigation-show">
+                            <span class="support-page-navigation-show__text">Показано:</span>
+                            <span class="support-page-navigation-show__count">50 из 120</span>
                         </span>
             </div>
         </main>
@@ -62,28 +68,23 @@ const tabs = [
     },
     {
         id: 1,
-        name: 'В обработке',
+        name: 'Активные',
     },
     {
         id: 2,
-        name: 'Выполнены',
+        name: 'Завершённые',
     },
 ];
-const views: Ref<string> = ref('rows');
 const activeTab: Ref<number> = ref(0);
 
-const changeViews = (value: string) => {
-    views.value = value
-}
 const classesTabs = (isChecked: boolean) => ({
-    'orders-tabs__wrap--active': isChecked,
+    'support-tabs__wrap--active': isChecked,
 })
 const handleChoice = (value: number) => {
     activeTab.value = value;
 }
-const isCompactedView: ComputedRef<boolean> = computed(() => unref(views) === 'tiles');
 </script>
 
 <style scoped lang="scss">
-@import '@/pages/office/styles/orders-page.scss';
+@import '@/pages/office/styles/support-page.scss';
 </style>
