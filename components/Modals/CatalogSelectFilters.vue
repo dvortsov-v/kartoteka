7d919d018a5e89a0fe654780982c431f38bb9cb3
vladislav_dvortsov <dvortsov.vlad@gmail.com>
2023-10-31 21:34:31 +0300

    Модалка фильтра

--- components/Modals/CatalogSelectFilters.vue
+++ components/Modals/CatalogSelectFilters.vue
@@ -1,14 +1,119 @@
 <template>
-    <VueFinalModal>
-        <div class="modal-catalog-filters">
-            <div class="catalog-page-main__filters-top">
-                <span class="catalog-page-main__filters-name">Фильтр</span>
-                <button @click="emit('close')" class="catalog-page-main__filters-close">
+    <VueFinalModal  contentClass="modal-catalog-filters">
+        <div class="modal-catalog-filters__content">
+            <div class="modal-catalog-filters__header">
+                <span class="modal-catalog-filters__name">Фильтр</span>
+                <button @click="emit('close')" class="modal-catalog-filters__close">
                     <svg-icon name="close"/>
                 </button>
             </div>
-            <CatalogCategories :categories="categoriesStore.categories" class="catalog-page-main__categories" />
-            <CatalogFilters @submitFilters="submitFilters" class="catalog-page-main__filters" />
+            <div class="modal-catalog-filters__scrollbar">
+                <CatalogCategories :categories="categoriesStore.categories" />
+                <div class="modal-catalog-filters__section">
+                    <h6 class="modal-catalog-filters__title modal-catalog-filters-title">Цена, ₽</h6>
+                    <div class="modal-catalog-filters__fields modal-catalog-filters-fields">
+                        <div class="modal-catalog-filters-fields__field">
+                            <UiInput type="number" placeholder="от" class="modal-catalog-filters-fields__input" />
+                        </div>
+                        <div class="modal-catalog-filters-fields__field">
+                            <UiInput type="number" placeholder="до" class="modal-catalog-filters-fields__input" />
+                        </div>
+                    </div>
+                    <ul class="modal-catalog-filters__list modal-catalog-filters-list">
+                        <li class="modal-catalog-filters-list__item">
+                            <UiChoices
+                                v-model:checked="formData.has_image"
+                                class="modal-catalog-filters-checkbox"
+                            >
+                        <span class="modal-catalog-filters-checkbox__text">
+                            Только с фото
+                        </span>
+                            </UiChoices>
+                        </li>
+                        <li class="modal-catalog-filters-list__item">
+                            <UiChoices
+                                v-model:checked="formData.is_lot"
+                                class="modal-catalog-filters-checkbox"
+                            >
+                        <span class="modal-catalog-filters-checkbox__text">
+                            В составе лота
+                        </span>
+                                <div class="modal-catalog-filters__hint"></div>
+                            </UiChoices>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modal-catalog-filters__section">
+                    <h6 class="modal-catalog-filters__title modal-catalog-filters-title">Статус объекта</h6>
+                    <ul class="modal-catalog-filters__list modal-catalog-filters-list">
+                        <li
+                            v-for="status in listStatus"
+                            :key="`catalog-page-status-${status.id}`"
+                            class="modal-catalog-filters-list__item"
+                        >
+                            <UiChoices
+                                v-model:checked="formData.status"
+                                name="status"
+                                :value="status.value"
+                                :checked="formData.status.includes(status.name)"
+                                class="modal-catalog-filters__checkbox modal-catalog-filters-checkbox"
+                            >
+                        <span class="modal-catalog-filters-checkbox__text">
+                            {{ status.name }}
+                        </span>
+                            </UiChoices>
+                        </li>
+                    </ul>
+                    <UiButtonLink
+                        v-if="false"
+                        class="modal-catalog-filters__more modal-catalog-filters-more"
+                    >
+                        <svg-icon name="adding" class="modal-catalog-filters-more__icon" />
+                        <span class="modal-catalog-filters-more__text">
+                    Показать еще
+                </span>
+                    </UiButtonLink>
+                </div>
+                <div class="modal-catalog-filters__section">
+                    <h6 class="modal-catalog-filters__title modal-catalog-filters-title">Период торгов</h6>
+                    <div class="modal-catalog-filters__fields modal-catalog-filters-fields">
+                        <div class="modal-catalog-filters-fields__field">
+                            <UiDatePicker v-model="formData.bargaining_to" class="modal-catalog-filters-fields__input"></UiDatePicker>
+                        </div>
+                        <div class="modal-catalog-filters-fields__field">
+                            <UiDatePicker v-model="formData.bargaining_from" class="modal-catalog-filters-fields__input"></UiDatePicker>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-catalog-filters__section">
+                    <h6 class="modal-catalog-filters__title modal-catalog-filters-title">Регион имущества</h6>
+                    <UiMultiSelect
+                        v-model="formData.region_ids"
+                        :options="regions"
+                        multiple
+                        placeholder="Все"
+                        trackBy="id"
+                        label="name"
+                        class="modal-catalog-filters__selector"
+                    />
+                </div>
+                <div class="modal-catalog-filters__bottom">
+                    <UiButton
+                        @click.prevent="submitFilters(paramsSubmit)"
+                        type="submit"
+                        class="modal-catalog-filters__more"
+                    >
+                        Показать {{ countProduct }} товаров
+                    </UiButton>
+                    <UiButtonLink
+                        @click.prevent="resetForm"
+                        type="reset"
+                        class="modal-catalog-filters__more"
+                    >
+                        Сбросить
+                    </UiButtonLink>
+                </div>
+            </div>
         </div>
     </VueFinalModal>
 </template>
@@ -16,15 +121,98 @@
 <script setup lang="ts">
 import { VueFinalModal } from 'vue-final-modal'
 import {useCategoriesStore} from "~/store/useCategoriesStore";
-import {ParamsProduct} from "~/api/ProductsApi";
-const categoriesStore = useCategoriesStore()
+import {useRegions} from "~/composable/request/useRegions";
+import {getProductsCountRequest, ParamsProduct} from "~/api/ProductsApi";
+import {LocationQueryValue} from "vue-router";
 
 const emit = defineEmits(['close', 'submitFilters']);
+const route = useRoute();
+const categoriesStore = useCategoriesStore()
+const {regions, getRegions} = useRegions();
+await getRegions();
+
+const listStatus = ref<{id: number, name: string, value: string}[]>([
+    {
+        id: 0,
+        name: 'Проведена инвентаризация',
+        value: 'Проведена инвентаризация',
+    },
+    {
+        id: 1,
+        name: 'Проведена оценка',
+        value: 'Проведена оценка',
+    },
+    {
+        id: 2,
+        name: 'Объявлены торги',
+        value: 'Объявлены торги',
+    },
+    {
+        id: 3,
+        name: 'Идут торги',
+        value: 'Идут торги',
+    },
+    {
+        id: 4,
+        name: 'Торги завершены, имущество не продано',
+        value: 'Торги завершены, имущество не продано',
+    },
+])
+const countProduct = ref(0);
+const formData = ref<ParamsProduct>({
+    has_image: false,
+    is_lot:  false,
+    region_ids: [],
+    bargaining_to: '',
+    bargaining_from: '',
+    status: [],
+    category_ids: route.params.id
+});
+
+const paramsSubmit = computed(() => {
+    return Object.entries(unref(formData)).reduce((acc: {[x: string] : number | boolean | string[] | number[] | LocationQueryValue | LocationQueryValue[]}, current: [string, (number | boolean | string[] | LocationQueryValue | LocationQueryValue[])]) => {
+        if(Array.isArray(current[1]) && (current[1].length > 0) && current[0] === 'region_ids' ) {
+            acc[current[0]] = current[1].map((item) => item.id).join(',');
+        } else if(current[1] && (typeof current[0] === 'string')) {
+            acc[current[0]] = current[1];
+        }
+
+        return acc;
+    }, {})
+})
+const resetForm = () => {
+    formData.value = {
+        has_image: false,
+        is_lot:  false,
+        region_ids: [],
+        bargaining_to: '',
+        bargaining_from: '',
+        status: [],
+        category_ids: route.params.id
+    }
+
+    emit('submitFilters', unref(paramsSubmit));
+}
+
+watch(paramsSubmit, async (params) => {
+    countProduct.value = await getProductsCountRequest(params);
+}, {immediate: true})
+
 const submitFilters = (params: ParamsProduct) => {
-    emit('submitFilters', params)
+    emit('submitFilters', {...params, page: null});
+    emit('close');
 }
 </script>
 
 <style scoped lang="scss">
 @import '@/components/Modals/styles/modal-catalog-filters.scss';
+</style>
+<style>
+.modal-catalog-filters {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+}
 </style>
